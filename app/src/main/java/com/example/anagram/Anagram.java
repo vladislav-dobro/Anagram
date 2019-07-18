654049fd59c0ac43521ae7d35262b798f17b3810
vladislav-dobro <vladislav_dobro@protonmail.com>
2019-07-18 14:35:52 +0500

    new class for anagram's builder

---
+++ app/src/main/java/com/example/anagram/Anagram.java
@@ -0,0 +1,57 @@
+package com.example.anagram;
+
+public class Anagram {
+
+    public static String getAnagram(String source) {
+
+        StringBuilder result =  new StringBuilder();
+
+        for (String word: source.split(" ")) {
+
+            int size = word.length();
+            char tmp;
+            char[] characters = word.toCharArray();
+
+            int left = 0;
+            int right = size - 1;
+
+            while (left < right) {
+
+                if (!Character.isLetter(characters[left]) &&
+                        Character.isLetter(characters[right])) {
+
+                    while (!Character.isLetter(characters[left]) && left < right) {
+                        left++;
+                    }
+
+                } else if (Character.isLetter(characters[left]) &&
+                        !Character.isLetter(characters[right])) {
+
+                    while (!Character.isLetter(characters[right]) && left < right) {
+                        right--;
+                    }
+
+                }
+
+                if (Character.isLetter(characters[left]) &&
+                        Character.isLetter(characters[right]) && left < right) {
+
+                    tmp = characters[left];
+                    characters[left] = characters[right];
+                    characters[right] = tmp;
+
+                }
+
+                right--;
+                left++;
+            }
+            if (result.toString().isEmpty()) {
+                result.append(new String(characters));
+            } else {
+                result.append(" " + new String(characters));
+            }
+        }
+
+        return result.toString();
+    }
+}
